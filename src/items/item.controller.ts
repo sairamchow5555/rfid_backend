f69adb81a90e295d325649a20fa94baab3c2636f
Sairam Chundru <sairam.chundru1432@gmail.com>
2024-09-06 12:31:35 +0530

    commit

--- src/items/item.controller.ts
+++ src/items/item.controller.ts
@@ -1,33 +1,42 @@
-import { Controller, Post, Body, Get, Param, Delete, Put } from "@nestjs/common";
-import { ItemService } from "./item.service";
-import { ItemModel } from "./dto/item-model";
-import { ItemReq } from "./dto/item-req";
-import { ItemIdReq } from "./dto/item-id.req";
-import { ItemReponseModel } from "./dto/item-response-model";
-import { ApiTags } from "@nestjs/swagger";
+import {
+    Body,
+    Controller,
+    Delete,
+    Param,
+    Post,
+    Put
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ItemIdReq } from './dto/item-id.req';
+import { ItemModel } from './dto/item-model';
+import { ItemReponseModel } from './dto/item-response-model';
+import { ItemService } from './item.service';
 
 @ApiTags('items')
 @Controller('items')
 export class ItemController {
-    constructor(private readonly itemService: ItemService) {}
+  constructor(private readonly itemService: ItemService) {}
 
-    @Post('create')
-    async createItem(@Body() dto: ItemModel): Promise<ItemReponseModel> {
-        return await this.itemService.createItem(dto);
-    }
+  @Post('create')
+  async createItem(@Body() dto: ItemModel): Promise<ItemReponseModel> {
+    return await this.itemService.createItem(dto);
+  }
 
-    @Put('update/:id')
-    async updateItem(@Param('id') id: number, @Body() dto: ItemModel): Promise<ItemReponseModel> {
-        return await this.itemService.updateItem(id, dto);
-    }
+  @Put('update/:id')
+  async updateItem(
+    @Param('id') id: number,
+    @Body() dto: ItemModel,
+  ): Promise<ItemReponseModel> {
+    return await this.itemService.updateItem(id, dto);
+  }
 
-    @Get('all-active')
-    async getAllItems(): Promise<ItemReponseModel> {
-        return await this.itemService.getAllItems();
-    }
+  @Post('allActive')
+  async getAllItems(): Promise<ItemReponseModel> {
+    return await this.itemService.getAllItems();
+  }
 
-    @Delete('delete')
-    async deleteItem(@Body() req: ItemIdReq): Promise<ItemReponseModel> {
-        return await this.itemService.deleteItem(req);
-    }
+  @Delete('delete')
+  async deleteItem(@Body() req: ItemIdReq): Promise<ItemReponseModel> {
+    return await this.itemService.deleteItem(req);
+  }
 }
